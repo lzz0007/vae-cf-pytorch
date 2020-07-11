2f8cbccdeead659a60cf2151dfb167a3bf8ac668
lingzi <47054314+lzz0007@users.noreply.github.com>
2020-07-11 10:23:37 +0800

    update

--- main_mvae.py
+++ main_mvae.py
@@ -251,7 +251,7 @@ def train():
                         elapsed * 1000 / args.log_interval,
                         train_loss / args.log_interval))
             for n,p in model.named_parameters():
-                if p.requires_grad:
+                if p.requires_grad and n == 'fc1_enc.weight':
                     print(n)
                     print(p)
             # Log loss to tensorboard
